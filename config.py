aea56e8ed136fb928fe931857015a0a5b37af2e0
Alex Lesan <alex.lesan@gmail.com>
2020-03-28 23:12:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
-read_from_dir = "C:/Users/ALESAN/Downloads"
-zip_path = "C:/Users/ALESAN/Downloads/archives/zip"
-rar_path = "C:/Users/ALESAN/Downloads/archives/rar"
+# path to director from where to read compressed files
+read_from_dir = "C:/Users/username/Downloads"
+
+# path where zip files will be unziped
+zip_path = "C:/Users/username/Downloads/archives/zip"
+
+# path where rar files will be unrar
+rar_path = "C:/Users/username/Downloads/archives/rar"
+
+# file extension that will be treated
 files_extensions = ('.zip', '.rar')
+
+# set it to True if want that compressed file to be deleted after unzip.
 delete_source_file = False
